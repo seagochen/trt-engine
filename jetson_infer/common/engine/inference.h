bcd9c97e42a4965827c5c06ca6b12c70ad6499a9
Orlando Chen <seagochen@hotmail.com>
2024-09-16 20:58:34 +0800

    新APを利用し、ソースコードを更新されました。

--- jetson_infer/common/engine/inference.h
+++ jetson_infer/common/engine/inference.h
@@ -1,13 +1,14 @@
 //
-// Created by ubuntu on 9/12/24.
+// Created by vipuser on 9/13/24.
 //
 
-#ifndef JETSON_INFER_YOLOV8_H
-#define JETSON_INFER_YOLOV8_H
+#ifndef JETSON_INFER_INFERENCE_H
+#define JETSON_INFER_INFERENCE_H
 
 #include <vector>
 #include <opencv2/opencv.hpp>
 #include <simple_cuda_toolkits/tensor.hpp>
+
 #include "common/nlohmann/json.hpp"
 
 
@@ -19,24 +20,8 @@ struct YoloPoint {
 struct YoloResult {
     int lx, ly, rx, ry, cls;
     float conf;
-    std::vector<YoloPoint> keypoints;
+    std::vector<YoloPoint> key_pts;
 };
-
-/**
- * @brief Initialize the CUDA temporary buffer
- *
- * @param width
- * @param height
- * @param channels
- */
-void initCudaTemporaryBuffer(int width, int height, int channels=3);
-
-
-/**
- * @brief Release the CUDA temporary buffer
- *
- */
-void releaseCudaTemporaryBuffer();
 
 
 /**
@@ -44,40 +29,47 @@ void releaseCudaTemporaryBuffer();
  *
  * @param image
  * @param output
+ * @param inputDim
  */
-void preprocess(cv::Mat &image, CudaTensor<float> &output);
+#include <map>
+void preprocess(cv::Mat &image, Tensor<float> &output);
 
 
 /**
  * @brief Postprocess the output
  *
  * @param input
- * @param output
+ * @param outputA¥  1`
  * @param confidence
  * @param model
  */
-void postprocess(const Tensor<float> &input, std::vector<YoloResult> &output,
-                 float confidence, std::string model="yolov8");
+void postprocess(const Tensor<float> &input, std::vector<YoloResult> &output, float confidence);
 
 
 
 // YoloPoint のシリアライズ関数
 void to_json(const YoloPoint& p, nlohmann::json& j);
 
+
 // YoloResult のシリアライズ関数
 void to_json(const YoloResult& r, nlohmann::json& j);
+
 
 // YoloResult のデシリアライズ関数
 void from_json(const std::string &str, YoloResult &r);
 
+
 // YoloPoint のデシリアライズ関数
 void from_json(const std::string &str, YoloPoint &p);
 
+
 // vector<YoloResult> のシリアライズ関数
 std::string to_json(const std::vector<YoloResult>& results);
+
 
 // vector<YoloResult> のデシリアライズ関数
 void from_json(const std::string &str, std::vector<YoloResult> &results);
 
 
-#endif //JETSON_INFER_YOLOV8_H
+
+#endif //JETSON_INFER_INFERENCE_H
